b403ea715c7b2d49710515769e9eab6acc586f87
tlskope <128537840+tlskope@users.noreply.github.com>
2023-12-15 10:03:57 +1300

    changes

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
 
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -25,7 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Error: Invalid Month")
 
     redirect_month = months[month -1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
