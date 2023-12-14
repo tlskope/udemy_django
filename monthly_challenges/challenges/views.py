c043771cbcb5aabaa373c75dd5d023c55b8a76c5
tlskope <128537840+tlskope@users.noreply.github.com>
2023-12-14 17:12:47 +1300

    all months added

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,17 +3,28 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "This is the january challenge.",
+    "february": "This is the february challenge.",
+    "march": "This is the march challenge.",
+    "april": "This is the april challenge.",
+    "may": "This is the may challenge.",
+    "june": "This is the june challenge.",
+    "july": "This is the july challenge.",
+    "august": "This is the august challenge.",
+    "september": "This is the september challenge.",
+    "october": "This is the october challenge.",
+    "november": "This is the november challenge.",
+    "december": "This is the december challenge.",
+}
+
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no meat for the month."
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes every day."
-    elif month == "march":
-        challenge_text = "Learn Django for at least 20 minutes every day."
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported.")
+    
