a4a1156412a4c4e2a5cb4fff232143448e7b493e
tlskope <128537840+tlskope@users.noreply.github.com>
2023-12-14 15:53:38 +1300

    latest updates

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def january(request):
     return HttpResponse("Eat no meat for the month")
 
-def february():
+def february(request):
     return HttpResponse("Walk for at least 20 minutes every day")
