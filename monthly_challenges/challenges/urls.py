d48c700f533aaaf7739413045baf8b6e6560f1dc
tlskope <128537840+tlskope@users.noreply.github.com>
2023-12-14 17:01:43 +1300

    app changes

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.february)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge),
 ]
 
